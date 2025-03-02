ca12614f76ed49bf87177e31963755d6be619393
ivan0811 <ivanfaathirza@gmail.com>
2025-03-02 22:08:48 +0700

    update evaluation

--- src/app/video-learning/[slug]/quiz/[type]/[id]/_component/Question.tsx
+++ src/app/video-learning/[slug]/quiz/[type]/[id]/_component/Question.tsx
@@ -5,7 +5,6 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { TQuizData } from "../page";
 
 type Props = {
   quizData: TQuizData | undefined;
@@ -13,6 +12,11 @@ type Props = {
   eventType: string;
   onDataFromQuestion?: (data: { questionId: number; answerId: number }) => void;
   onSelectedPagination?: (page: number | undefined) => void;
+  options: {
+    redirectPagination: string;
+    redirectCompleted: string;
+    pathType: string;
+  };
 };
 
 export const Question = ({
@@ -20,18 +24,39 @@ export const Question = ({
   onDataFromQuestion,
   question,
   quizData,
+  options,
 }: Props) => {
   const [selectedAnswer, setSelectedAnswer] = useState<string>("");
+
+  function stylePagination(item: { current: boolean; completed: boolean }) {
+    if (options.pathType === "quiz") {
+      return item.current
+        ? "bg-[#3A5C9B] border border-[#4B6FBF] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]"
+        : item.completed
+        ? "bg-[#1E824C] border border-[#27AE60] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]"
+        : "bg-primary border-[#2D4C7C] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]";
+    }
+
+    if (options.pathType === "evaluation") {
+      return item.current
+        ? "bg-[#616161] border border-[#BDBDBD] hover:bg-[#90A4AE] hover:border-[#BDBDBD]"
+        : "bg-[#424242] border-[#757575] hover:bg-[#90A4AE] hover:border-[#BDBDBD]";
+    }
+
+    return "";
+  }
+
+  function styleAnswer() {}
 
   // Sinkronisasi state jika question berubah
   useEffect(() => {
     if (question?.userAnswer?.[0]?.answerId) {
-      const getDataAnswer = question.userAnswer[0].answerId
+      const getDataAnswer = question.userAnswer[0].answerId;
       setSelectedAnswer(getDataAnswer.toString());
       onDataFromQuestion?.({
         questionId: question.id,
-        answerId: getDataAnswer
-      })
+        answerId: getDataAnswer,
+      });
     } else {
       setSelectedAnswer(""); // Reset jika tidak ada jawaban
     }
@@ -51,25 +76,40 @@ export const Question = ({
           </div>
           <div className="mt-5">
             <div className="flex flex-col">
-              <RadioGroup
-                value={selectedAnswer}
-                onValueChange={(e) => {
-                  setSelectedAnswer(e);
-                  onDataFromQuestion?.({
-                    questionId: question?.id || 0,
-                    answerId: parseInt(e),
-                  });
-                }}
-              >
-                {question?.Answer.map((it, index) => (
-                  <div key={it.id} className="flex items-center space-x-2">
-                    <RadioGroupItem value={it.id.toString()} id={"r" + index} />
-                    <Label className="text-2xl my-4 w-full" htmlFor={"r" + index}>
-                      <div className="cursor-pointer">{it.answerText}</div>
-                    </Label>
-                  </div>
-                ))}
-              </RadioGroup>
+              {
+                options.pathType === 'evaluation' &&
+                <div>
+                  
+                </div>
+              }
+              {
+                options.pathType === 'quiz' &&
+                <RadioGroup
+                  value={selectedAnswer}
+                  onValueChange={(e) => {
+                    setSelectedAnswer(e);
+                    onDataFromQuestion?.({
+                      questionId: question?.id || 0,
+                      answerId: parseInt(e),
+                    });
+                  }}
+                >
+                  {question?.Answer.map((it, index) => (
+                    <div key={it.id} className="flex items-center space-x-2">
+                      <RadioGroupItem
+                        value={it.id.toString()}
+                        id={"r" + index}
+                      />
+                      <Label
+                        className="text-2xl my-4 w-full"
+                        htmlFor={"r" + index}
+                      >
+                        <div className="cursor-pointer">{it.answerText}</div>
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              }
             </div>
           </div>
         </div>
@@ -83,14 +123,9 @@ export const Question = ({
                 key={item.number}
                 onClick={() => onSelectedPagination?.(item.number)}
                 className={`m-0 bg-[#2E2E2E] border 
-                  ${
-                    item.current
-                      ? "bg-[#3A5C9B] border border-[#4B6FBF] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]"
-                      : item.completed
-                      ? "bg-[#1E824C] border border-[#27AE60] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]"
-                      : "bg-primary border-[#2D4C7C] hover:bg-[#2D4C7C] hover:border-[#3A5C9B]"
-                  } }`}
+                  ${stylePagination(item)} }`}
               >
+                
                 {item.number}
               </Button>
             )
--- src/app/video-learning/[slug]/quiz/[type]/page.tsx
+++ src/app/video-learning/[slug]/quiz/[type]/page.tsx
@@ -5,27 +5,50 @@ import { notFound } from "next/navigation";
 import { ApiResponse, fetchApi } from "@/lib/utils/fetchApi";
 
 export const metadata: Metadata = {
-  title: 'Mindo Class | Quiz'
-}
+  title: "Mindo Class | Quiz",
+};
+
+export type TQuiz =
+  | {
+      id: number;
+      title: string;
+      limitTrial: number;
+      limitTime: string;
+      type: string;
+      pagination: boolean;
+      random: boolean;
+      quizAttempt: Array<{
+        score: number;
+        _count: {
+          UserAnswer: number;
+        };
+      }>;
+      _count: {
+        question: number;
+      };
+    }
+  | undefined;
 
 type Props = {
   params: {
-    [key: string]: string
-  }
-}
+    [key: string]: string;
+  };
+};
 
 const Page = async (props: Props) => {
-  const getSlug = props.params.slug as string || ""
-  const getType = props.params.type as string || ""
-  const quiz: ApiResponse = await fetchApi(`/quiz/${getSlug}/event/${getType}`);
-  
-  if(quiz && !quiz.success || !quiz){
-    notFound()
+  const getSlug = (props.params.slug as string) || "";
+  const getType = (props.params.type as string) || "";
+  const quiz: ApiResponse<TQuiz> = await fetchApi(
+    `/quiz/${getSlug}/event/${getType}`
+  );
+
+  if ((quiz && !quiz.success) || !quiz) {
+    notFound();
   }
 
   return (
     <div>
-      <AttemptQuiz quiz={quiz?.data}/>
+      <AttemptQuiz quiz={quiz?.data} slug={getSlug} type={getType} />
     </div>
   );
 };
