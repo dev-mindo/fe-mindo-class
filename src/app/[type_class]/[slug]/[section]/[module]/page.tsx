09c13502b2fe25368a2787ad12853a847cd08589
ivan0811 <ivanfaathirza@gmail.com>
2025-03-04 00:48:20 +0700

    add consume api

---
+++ src/app/[type_class]/[slug]/[section]/[module]/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import { ModuleMaterial } from "./_component/video/ModuleMaterial";
+import { ApiResponse, fetchApi } from "@/lib/utils/fetchApi";
+import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Mindo Class | Module",
+};
+
+type Props = {
+  params: {
+    [key: string]: string;
+  };
+  searchParams: {
+    [key: string]: string;
+  };
+};
+
+export default async function Page({ params, searchParams }: Props) {
+  const getTypeClass = params.type_class;
+  let getSection = params.section;
+  const getSlug = params.slug;
+  if (getTypeClass === "video-learning") getSection = getSlug;
+  const getModuleSlug = params.module;
+  const getModule: ApiResponse<TModuleMaterial> = await fetchApi(
+    `/classroom/${getSection}/${getModuleSlug}`
+  );
+  if ((getModule && !getModule.success) || !getModule) notFound();
+
+  return <ModuleMaterial materialData={getModule.data} />;
+}
