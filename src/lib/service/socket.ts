946d6decd6cefc22fe02c99fddac046e64b4031b
ivan0811 <ivanfaathirza@gmail.com>
2025-04-16 02:33:25 +0700

    update url socket

--- src/lib/service/socket.ts
+++ src/lib/service/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://mindo-socket-service-swjti.ondigitalocean.app' : 'http://localhost:3004';
+// const URL = process.env.NODE_ENV === 'production' ?  : 'http://localhost:3004';
 
-export const socket = io(URL);
+export const socket = io('mindo-socket-service-swjti.ondigitalocean.app', {
+    transports: ["websocket"]
+});
