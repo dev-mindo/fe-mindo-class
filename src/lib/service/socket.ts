3647870f54f029b3911256a992094001706b87a0
ivan0811 <ivanfaathirza@gmail.com>
2025-04-17 17:38:22 +0700

    fix socket

--- src/lib/service/socket.ts
+++ src/lib/service/socket.ts
@@ -4,5 +4,8 @@ import { io } from 'socket.io-client';
 // const URL = process.env.NODE_ENV === 'production' ?  : 'http://localhost:3004';
 
 export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-    transports: ["websocket"]
+    transports: ["websocket"],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
 });
